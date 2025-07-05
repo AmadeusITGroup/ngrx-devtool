1d32bb44f9bc8d4619e8519654ce0eef87d2ba10
adam almounayar <adam.almounayar@amadeus.com>
2025-07-05 21:53:37 +0200

    feat(ui): setup angular material and display actions

--- projects/ngrx-devtool-ui/src/app/app.component.ts
+++ projects/ngrx-devtool-ui/src/app/app.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, OnInit, signal, computed } from '@angular/core';
+import { WebsocketService } from '../services/websocket.service';
+import { Subscription } from 'rxjs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTabsModule } from '@angular/material/tabs';
+import { JsonTreeComponent } from '../components/json-tree/json-tree.component';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    DatePipe,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatExpansionModule,
+    MatTabsModule,
+    JsonTreeComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngrx-devtool-ui';
+  messages = signal<any[]>([]);
+  private subscription?: Subscription;
+  constructor(private _webSocketService: WebsocketService) {}
+  ngOnInit(): void {
+    this._webSocketService.connect('ws://localhost:4000');
+    this.subscription = this._webSocketService.messages$?.subscribe((msg) =>
+      this.messages.update((arr) => [...arr, msg])
+    );
+  }
+  ngOnDestroy() {
+    this._webSocketService.close();
+    this.subscription?.unsubscribe();
+  }
 }
---
+++ projects/ngrx-devtool-ui/src/components/json-tree/json-tree.component.ts
@@ -0,0 +1,16 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
+export interface JsonTreeNode {
+  key: string;
+  value?: string | number | boolean | null;
+  children?: JsonTreeNode[];
+}
+@Component({
+  selector: 'json-tree',
+  imports: [],
+  templateUrl: './json-tree.component.html',
+  styleUrl: './json-tree.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class JsonTreeComponent{
+  @Input() jsonData: any;
+}
